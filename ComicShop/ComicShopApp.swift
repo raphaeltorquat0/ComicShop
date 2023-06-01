0269db4d3a7b3d1616ca66f0647c6ac85b1a57b2
Raphael Torquato <raphael.martini@live.com>
2023-06-01 17:49:24 -0300

    Initial Commit

---
+++ ComicShop/ComicShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicShopApp.swift
+//  ComicShop
+//
+//  Created by Raphael Torquato on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ComicShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
